c77424bcd6e2069eada94d24cf47de8ec786a5e5
Reign-Beaux <samc920130@hotmail.com>
2023-09-21 22:51:47 -0600

    Se crearon los templates para el body

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,13 +1,14 @@
 import { useAppContext } from "@/context";
 import React from "react";
 import "./style.css";
+import { AboutMe, Articles } from "./components";
 
 export interface BodyProps {}
 
 const Body: React.FC<BodyProps> = ({}) => {
   const { themeSelected } = useAppContext();
 
-  return <div className="body">{themeSelected}</div>;
+  return <div className="body">{!!themeSelected ? <Articles /> : <AboutMe />}</div>;
 };
 
 export default Body;
---
+++ src/components/Body/components/Articles/Articles.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface ArticlesProps {
+}
+
+const Articles: React.FC<ArticlesProps>  = ({}) => {
+	return <div>Articles</div>;
+};
+
+export default Articles;

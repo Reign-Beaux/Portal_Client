a76641ed3f1d339ccfe502afbb4b03dd659f7b35
Reign-Beaux <samc920130@hotmail.com>
2023-09-23 16:41:05 -0600

    Se agrego el provider

--- src/components/Body/components/Articles/Articles.tsx
+++ src/components/Body/components/Articles/Articles.tsx
@@ -1,7 +1,8 @@
 import { useAppContext } from "@/context";
 import React from "react";
-import { ArticleCards } from "./components";
+import { ArticleCards, ArticleDialog } from "./components";
 import "./style.css";
+import { ArticleProvider } from "./context";
 
 export interface ArticlesProps {}
 
@@ -9,12 +10,13 @@ const Articles: React.FC<ArticlesProps> = ({}) => {
   const { themeSelected } = useAppContext();
 
   return (
-    <>
+    <ArticleProvider>
       <div style={{ height: "40px", textAlign: "center" }}>
         <h1 style={{ margin: 0 }}> {themeSelected} </h1>
       </div>
       <ArticleCards />
-    </>
+      <ArticleDialog />
+    </ArticleProvider>
   );
 };
 
